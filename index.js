54a37f989d46a1c98725570ba8cf0087bdd59894
Mohammad Moslem Uddin <pal.uddin@gmail.com>
2024-12-09 20:44:33 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let myLeads = []
+
+
+const leadsInput = document.getElementById("leads-input") 
+const saveBtn = document.getElementById("save-btn")
+const displayLink = document.getElementById("display-link")
+const dltBtn = document.getElementById("dlt-btn")
+
+saveBtn.addEventListener("click", function(){
+   myLeads.push(leadsInput.value) 
+   renderLeads()
+   leadsInput.value =""
+})
+
+dltBtn.addEventListener("click", function(){
+    displayLink.remove()
+    
+})
+
+
+
+function renderLeads(){
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++){
+        listItems += `<li> ${myLeads[i]} </li>`
+
+    }
+
+displayLink.innerHTML = listItems
+}
